16ed257cfc1f0e30b08c4d15f0aba0951b59cd2d
Bastien FUCHS <https://github.com/FuchsBastien/FuchsBastien_5_07092021.git>
2021-11-08 14:46:07 +0100

    modif controller article

--- backend/controllers/article.js
+++ backend/controllers/article.js
@@ -1,55 +1,55 @@
-const Sauce = require('../models/article');
+const Article = require('../models/article');
 const fs = require('fs');
 
 
-//enregistrer nouvelle sauce
-exports.createSauce = (req, res, next) => {
-    const sauceObject = JSON.parse(req.body.sauce);
+//enregistrer nouvel article
+exports.createArticle = (req, res, next) => {
+    const articleObject = JSON.parse(req.body.article);
     //ID de le requête à supprimer car généré automatiquement par mongoDB
-    delete sauceObject._id;
-    // Création d'un nouvel objet Sauce à partir du modèle créé
-    const sauce = new Sauce({
-      //copie tous les champs de la requête de la variable sauceObject
-      ...sauceObject,
+    delete articleObject._id;
+    // Création d'un nouvel objet Article à partir du modèle créé
+    const article = new Article({
+      //copie tous les champs de la requête de la variable articleObject
+      ...articleObject,
       // Création de l'URL de l'image : http://localhost:3000/image/nomdufichier 
       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
-    // Enregistrement de l'objet sauce dans la base de données
-    sauce.save()
+    // Enregistrement de l'objet article dans la base de données
+    article.save()
       //réponse obligatoire sinon expiration de la requête
-      .then(() => res.status(201).json({ message: 'Sauce enregistré !'}))
+      .then(() => res.status(201).json({ message: 'Article enregistré !'}))
       //erreur si requête non envoyé au serveur
       .catch(error => res.status(400).json({ error }));
   };
 
 
-//modifier sauce selectionnée
-  exports.modifySauce = (req, res, next) => {
-    const sauceObject = req.file ?
+//modifier article selectionné
+  exports.modifyArticle = (req, res, next) => {
+    const articleObject = req.file ?
      // Si il existe déjà une image
     {
-      ...JSON.parse(req.body.sauce),
+      ...JSON.parse(req.body.article),
       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     } : { ...req.body };
       // Si il n'existe pas d'image
-    Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Sauce modifiée !'}))
+    Article.updateOne({ _id: req.params.id }, { ...articleObject, _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Article modifié !'}))
       .catch(error => res.status(400).json({ error }));
   };
   
 
-  //supprimer sauce selectionnée
-  exports.deleteSauce = (req, res, next) => {
+  //supprimer article selectionné
+  exports.deleteArticle = (req, res, next) => {
     //trouver l'objet dans la base de données
-    Sauce.findOne({ _id: req.params.id })
-      .then(sauce => {
+    Article.findOne({ _id: req.params.id })
+      .then(article => {
         // Récupération du nom du fichier
-        const filename = sauce.imageUrl.split('/images/')[1];
+        const filename = article.imageUrl.split('/images/')[1];
         // On efface le fichier (unlink)
         fs.unlink(`images/${filename}`, () => {
           //on supprime l'objet dans la base de données
-          Sauce.deleteOne({ _id: req.params.id })
-            .then(() => res.status(200).json({ message: 'Sauce supprimée !'}))
+          Article.deleteOne({ _id: req.params.id })
+            .then(() => res.status(200).json({ message: 'Article supprimée !'}))
             .catch(error => res.status(400).json({ error }));
         });
       })
@@ -57,19 +57,19 @@ exports.createSauce = (req, res, next) => {
   };
 
 
-//afficher sauce selectionnée
-  exports.getOneSauce = (req, res, next) => {
-    //afficher la sauce par son ID récupéré dans l'url
-    Sauce.findOne({ _id: req.params.id })
-      .then(sauce => res.status(200).json(sauce))
+//afficher article selectionnée
+  exports.getOneArticle = (req, res, next) => {
+    //afficher l'article par son ID récupéré dans l'url
+    Article.findOne({ _id: req.params.id })
+      .then(article => res.status(200).json(article))
       .catch(error => res.status(404).json({ error }));
   };
 
 
-//afficher toutes les sauces
-  exports.getAllSauce = (req, res, next) => {
-    Sauce.find()
-      .then(sauce => res.status(200).json(sauce))
+//afficher toutes les articles
+  exports.getAllArticle = (req, res, next) => {
+    Article.find()
+      .then(article => res.status(200).json(article))
       .catch(error => res.status(400).json({ error }));
   };
   
