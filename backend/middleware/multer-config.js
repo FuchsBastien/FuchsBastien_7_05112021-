2d55aa20d539cb92efb4d6852f8bd50687dd636e
Bastien FUCHS <https://github.com/FuchsBastien/FuchsBastien_5_07092021.git>
2021-11-08 12:08:06 +0100

    middleware multer-config

---
+++ backend/middleware/multer-config.js
@@ -0,0 +1,25 @@
+// Utilisation de multer pour enregistrer les fichiers images
+const multer = require('multer');
+
+// Modification de l'extension des fichiers
+const MIME_TYPES = {
+  'image/jpg': 'jpg',
+  'image/jpeg': 'jpg',
+  'image/png': 'png'
+};
+
+const storage = multer.diskStorage({
+  // Enregistrement dans le dossier "images"
+  destination: (req, file, callback) => {
+    callback(null, 'images');
+  },
+   // Génération du nom du fichier : nom d'origine + numero unique + . + extension
+  filename: (req, file, callback) => {
+    const name = file.originalname.split(' ').join('_');
+    const extension = MIME_TYPES[file.mimetype];
+    callback(null, name + Date.now() + '.' + extension);
+  }
+});
+
+//Exporter multer fichier unique image
+module.exports = multer({storage: storage}).single('image');
