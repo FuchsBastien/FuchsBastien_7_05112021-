8160a211fb42af96aee72ad1ff2ddfc3fa83216f
Bastien FUCHS <https://github.com/FuchsBastien/FuchsBastien_5_07092021.git>
2021-11-18 11:39:02 +0100

    correction put comment controller

--- backend/controllers/comment.js
+++ backend/controllers/comment.js
@@ -57,7 +57,7 @@ exports.createComment = (req, res, next) => {
 // logique métier : modifier un commentaire
 exports.modifyComment = (req, res, next) => {
   const commentObject = req.body
-   Comment.update({ where: {id: req.params.id}}, {...commentObject})
+   Comment.update({ ...commentObject, id:  req.params.id},{ where: {id: req.params.id}})
     .then(() => res.status(200).json({ message: 'Commentaire modifié !'}))
     .catch(error => res.status(400).json({ error }));
 };
